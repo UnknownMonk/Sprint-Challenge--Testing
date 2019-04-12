77641ff3ebd90264e582d7890d59ebdaccc85fc3
UnknownMonk <smodcartoon@gmail.com>
2019-04-12 11:55:10 -0400

    Added migration game model and db

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-// const games = require("../games/gamesModel");
+const games = require('../games/gamesModel');
 
 const server = express();
 
@@ -10,6 +10,42 @@ server.use(express.json());
 
 server.get('/', async (req, res) => {
   res.status(200).json({ api: 'Its go time' });
+});
+
+server.get('/api/games', async (req, res) => {
+  const gameList = await games.getAll();
+
+  if (gameList) {
+    res.status(200).json(gameList);
+  } else {
+    return res.status(500).json({ message: 'DO YOU THINK THIS IS A GAME!!' });
+  }
+});
+
+server.get('/api/games/:id', async (req, res) => {
+  try {
+    const game = await games.findById(req.params.id);
+    if (game) {
+      return res.status(200).json(game);
+    } else {
+      return res.status(404).json({ message: 'No games here ' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.post('/api/games', async (req, res) => {
+  try {
+    const game = await games.insert(req.body);
+    if (game) {
+      return res.status(200).json(game);
+    } else {
+      return res.status(404).json({ message: 'Add a game' });
+    }
+  } catch (error) {
+    return res.status(422).json({ message: 'Could not add a game' });
+  }
 });
 
 module.exports = server;
